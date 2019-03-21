621497b71583cada32a71d44f48f475c2cb92347
mifta04 <ditata456@gmail.com>
2019-03-21 19:01:33 +0700

    view pager yulva

---
+++ app/src/main/java/belajar1/yulva/com/belajar1/MainActivity.kt
@@ -0,0 +1,33 @@
+package belajar1.yulva.com.belajar1
+
+import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import com.ogaclejapan.smarttablayout.SmartTabLayout
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter = FragmentPagerItemAdapter(
+            supportFragmentManager, FragmentPagerItems.with(this)
+                .add("Home", satuFragment::class.java)
+                .add("Recomendasi Manga", duaFragment::class.java)
+                .add("Streaming Manga", tigaFragment::class.java)
+                .add("Profil", empatFragment::class.java)
+                .create()
+        )
+
+        val viewPager = findViewById(R.id.viewpager) as ViewPager
+        viewPager.adapter = adapter
+
+        val viewPagerTab = findViewById(R.id.viewpagertab) as SmartTabLayout
+        viewPagerTab.setViewPager(viewPager)
+
+    }
+
+    }
